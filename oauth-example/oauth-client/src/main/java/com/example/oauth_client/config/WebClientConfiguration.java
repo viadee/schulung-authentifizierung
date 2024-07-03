dec0b5cfbd0c23347e9a2f1b73c0fb3adf37af88
Martin Müller <martin.mueller@viadee.de>
2024-07-03 13:53:38 +0200

    Configuration für WebClient

---
+++ oauth-example/oauth-client/src/main/java/com/example/oauth_client/config/WebClientConfiguration.java
@@ -0,0 +1,16 @@
+package com.example.oauth_client.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfiguration {
+
+    @Bean
+    WebClient webClient() {
+        return WebClient.builder()
+                .build();
+    }
+
+}
