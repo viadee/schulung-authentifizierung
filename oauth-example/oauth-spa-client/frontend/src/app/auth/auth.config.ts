f53c7fe3ec6a7aa054350236baa6d18d66e601a9
Martin Müller <39087682+mum-viadee@users.noreply.github.com>
2024-11-20 09:43:43 +0100

    Scope "profile" entfernt

--- oauth-example/oauth-spa-client/frontend/src/app/auth/auth.config.ts
+++ oauth-example/oauth-spa-client/frontend/src/app/auth/auth.config.ts
@@ -6,7 +6,7 @@ export const authConfig: PassedInitialConfig = {
             redirectUrl: window.location.origin,
             postLogoutRedirectUri: window.location.origin,
             clientId: 'public-oauth-client',
-            scope: 'openid profile products:read',
+            scope: 'openid products:read',
             responseType: 'code',
             silentRenew: false,
             renewTimeBeforeTokenExpiresInSeconds: 10,
