e1811978acb45c960853304e8bea7f9b8fda2b53
Martin Müller <martin.mueller@viadee.de>
2026-05-29 15:01:04 +0200

    Blöcke für Übungen wieder kommentiert

--- oauth-example/oauth-spa-client/frontend/src/app/auth/auth.config.ts
+++ oauth-example/oauth-spa-client/frontend/src/app/auth/auth.config.ts
@@ -1,14 +1,14 @@
-import { PassedInitialConfig } from 'angular-auth-oidc-client';
+// import { PassedInitialConfig } from 'angular-auth-oidc-client';
 
-export const authConfig: PassedInitialConfig = {
-  config: {
-            authority: 'http://127.0.0.1:9000',
-            redirectUrl: window.location.origin,
-            postLogoutRedirectUri: window.location.origin,
-            clientId: 'public-oauth-client',
-            scope: 'openid products:read',
-            responseType: 'code',
-            silentRenew: false,
-            renewTimeBeforeTokenExpiresInSeconds: 10,
-        }
-}
+// export const authConfig: PassedInitialConfig = {
+//   config: {
+//             authority: 'http://127.0.0.1:9000',
+//             redirectUrl: window.location.origin,
+//             postLogoutRedirectUri: window.location.origin,
+//             clientId: 'public-oauth-client',
+//             scope: 'openid products:read',
+//             responseType: 'code',
+//             silentRenew: false,
+//             renewTimeBeforeTokenExpiresInSeconds: 10,
+//         }
+// }
